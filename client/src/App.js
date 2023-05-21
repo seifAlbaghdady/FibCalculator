0a5aae1d9660a9c4ea292afcf5e28ae6e38ed0a1
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2023-05-22 00:20:20 +0300

    Updated UI

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,19 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div className="app">
-        <header className="app-header">
-          <h1 className="app-title">Fib Calculator</h1>
-          <nav className="app-nav">
-            <Link to="/" className="app-link">Home</Link>
-            <Link to="/otherpage" className="app-link">Other Page</Link>
-          </nav>
-        </header>
-        <div className="app-content">
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
+      <div className="calculator">
+        <div className="app">
+          <header className="app-header">
+            <h1 className="app-title">Fib Calculator</h1>
+            <nav className="app-nav">
+              <Link to="/" className="app-link">Home</Link>
+              <Link to="/otherpage" className="app-link">Other Page</Link>
+            </nav>
+          </header>
+          <div className="app-content">
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </div>
       </div>
     </Router>
